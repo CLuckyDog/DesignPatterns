8bf42a48066491e094fe570e838f370f70e5aede
Alion <406662248@qq.com>
2019-08-13 21:21:47 +0800

    修改了点

--- src/main/java/com/rh/PrototypePattern/ShapeCache.java
+++ src/main/java/com/rh/PrototypePattern/ShapeCache.java
@@ -16,6 +16,7 @@ public class ShapeCache {
     // 对每种形状都运行数据库查询，并创建该形状
     // shapeMap.put(shapeKey, shape);
     // 例如，我们要添加三种形状
+    
     public static void loadCache() {
         Circle circle = new Circle();
         circle.setId("1");
