5c040e8acd4e10874e6d933930beaf5e699a26f5
Alion <406662248@qq.com>
2019-09-01 17:24:28 +0800

    提交测试

--- src/main/java/com/rh/SingletonPattern/SingleObject.java
+++ src/main/java/com/rh/SingletonPattern/SingleObject.java
@@ -6,7 +6,7 @@ package com.rh.SingletonPattern;
 public class SingleObject {
     //创建 SingleObject 的一个对象
     private static SingleObject instance = new SingleObject();
-
+ 
     //让构造函数为 private，这样该类就不会被实例化
     private SingleObject(){}
 
